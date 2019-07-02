ff83b12d3c35493deefc5a24e5d87e8d615dad6c
VihoSerge <komi.vihoserge@gmail.com>
2019-07-02 16:20:46 +0000

    #bubble_sort method implemented

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,16 @@
-linters:
-  rubocop:
-    display_cop_names: true
+def bubble_sort(arr)
+  loop do
+    swapped = false
+    for j in 0...arr.length - 1
+      if(arr[j] > arr[j+1])
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+        swapped = true
+      end
+    end
+    break if !swapped
+  end
+
+  arr
+end
+
+bubble_sort([4,3,78,2,0,2])
