e0ccc20aa8180cef443382810c37140738dca152
Redvan <redvanisation@gmail.com>
2019-07-02 12:57:15 -0400

    bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -9,8 +9,35 @@ def bubble_sort(arr)
     end
     break if !swapped
   end
-
   arr
 end
 
-bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(arr)    
+  loop do
+    swapped = false
+    for j in 0...arr.length - 1
+      result = yield(arr[j], arr[j+1])
+
+      if result > 0
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+        swapped = true
+      end
+    end
+    break if !swapped
+  end
+  arr
+end
+
+
+
+result = bubble_sort_by(["hi","hello", "hey"]) do |left,right|
+  left.length - right.length
+end
+
+
+
+p result
+
+
+puts bubble_sort([4,3,78,2,0,2])
